49461bc8a78ec6739348c71f3ef6dd44c4a26402
Mark <61974196+Marksman007577@users.noreply.github.com>
2023-11-09 15:10:56 +0100

    init commit

---
+++ Advanced Python/Advanced Python/String/main.py
@@ -0,0 +1,20 @@
+S = 'spam'
+print(S)
+
+for x in S: print(x)
+title = "Meaning", ' of' ," Statement"
+print(title)
+
+print(len('abc' + 'def'))
+
+print('Ni!' * 4)
+
+print('------- ...more... ---')
+
+print('-' * 80)
+
+myjob = "hacker"
+for c in myjob:
+    print(c, end=' \n')
+
+print(S[1:3], S[1:], S[:-1])
