a4c17c8c0373e7c9b85058579fc76db4ad2db9f6
Mark <61974196+Marksman007577@users.noreply.github.com>
2023-11-10 18:24:55 +0100

    file updated

--- Advanced Python/Advanced Python/String/main.py
+++ Advanced Python/Advanced Python/String/main.py
@@ -1,8 +1,9 @@
+# string basics
 S = 'spam'
 print(S)
 
-for x in S: print(x)
-title = "Meaning", ' of' ," Statement"
+for x in S:print(x)
+title = "Meaning", ' of', " Statement"
 print(title)
 
 print(len('abc' + 'def'))
@@ -13,8 +14,22 @@ print('------- ...more... ---')
 
 print('-' * 80)
 
-myjob = "hacker"
-for c in myjob:
+my_job = "hacker"
+for c in my_job:
     print(c, end=' \n')
 
 print(S[1:3], S[1:], S[:-1])
+
+# character code conversion
+v = ord('s')
+w = chr(115)
+print(v)
+print(w)
+
+# binary to base 10 conversion
+print(int('100110011', 2))
+print(bin(13))
+
+# string methods
+result = S.find('pa')
+print(bool(result))
